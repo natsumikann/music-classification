8558ea426b4b30070a4b4299148728f80e857718
Hiroe Ka <sakurasweets.panda@gmail.com>
2017-11-09 14:01:43 +0900

    test file correction done(need to be corrected again later)

--- test_soundnet.py
+++ test_soundnet.py
@@ -7,22 +7,25 @@ import chainer
 from Regressor import SoundNet5Layer
 from Dataset_ta import Dataset
 
+# TODO add test data
+# TODO complete dataset_args
+
 def main():
     parser = argparse.ArgumentParser(description='Practice: SoundNet5Layer')
-    parser.add_argument('--batchsize', '-b', type=int, default=100,
+    parser.add_argument('--batchsize', '-b', type=int, default=16,
                         help='Number of songs in each mini-batch')
     parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--model', '-m', default='result/sound_net_20',
+    parser.add_argument('--model', '-m', default='result/sound_net_1000',
                         help='Path to the model')
-    parser.add_argument('--dataset', '-d', default='mini_cifar/test',
+    parser.add_argument('--dataset', '-d', default='',
                         help='Directory for train sound_net')
     args = parser.parse_args()
 
     print('GPU: {}'.format(args.gpu))
     print('')
 
-    model = SoundNet5Layer(10)
+    model = SoundNet5Layer()
     chainer.serializers.load_npz(args.model, model)
 
     if args.gpu >= 0:
@@ -30,9 +33,9 @@ def main():
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()  # Copy the model to the GPU
 
-    # Load the Cifar-10 mini_cifar
+    # Load the soundnet
     # trainとvalに分ける
-    test = MyCifarDataset(args.dataset)
+    test = Dataset(args.dataset)
     print('test data : {}'.format(len(test)))
 
     test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
